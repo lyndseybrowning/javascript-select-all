609c2e7d115df1363e15d2537b6d6e01ba0d42cc
Lyndsey Browning <lbrowning86@gmail.com>
2016-03-23 19:51:39 +0000

    Added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const gulp = require('gulp');
+const esLint = require('gulp-eslint');
+const babel = require('gulp-babel');
+
+const paths = {
+  build: 'dist/',
+  js: 'src/**'
+};
+
+gulp.task('script', function() {
+  return gulp
+    .src(paths.js)
+    .pipe(esLint())
+    .pipe(esLint.format())
+    .pipe(babel())
+    .pipe(gulp.dest(paths.build))
+});
+
+gulp.task('default', ['script'], () => {
+  gulp.watch(paths.js, ['script']);
+});
